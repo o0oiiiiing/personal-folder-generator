47f5580cbded8cba7bace2e2124e4fe3de3ff4cc
o0oiiiiing <oing_0ing@naver.com>
2025-05-16 14:05:39 +0900

    ✨ feat: 생성할 폴더 리스트박스 및 추가, 삭제 버튼 레이아웃 구현

--- main.py
+++ main.py
@@ -34,22 +34,18 @@ class BackupFolderApp:
 
     # ───────────────────── 위젯 생성 ─────────────────────
     def create_widgets(self):
-        # 여백
-        blank_frame = tk.Frame(self.root)
-        blank_frame.pack(pady=10)
-        
         # 프레임
         select_frame = tk.Frame(self.root)
-        select_frame.pack(pady=5)
+        select_frame.pack(pady=(15, 0))
 
         # 폴더 선택
         folder_label = tk.Label(select_frame, text="생성할 위치 : ", font=self.app_font)
-        folder_label.grid(row=0, column=0, sticky=tk.W, padx=5)
+        folder_label.grid(row=0, column=0, sticky=tk.E, padx=5)
 
         self.folder_entry = tk.Entry(select_frame, font=self.app_font, width=30)
         self.folder_entry.config(relief="sunken")
         self.folder_entry.insert(0, "폴더를 선택하세요")
-        self.folder_entry.grid(row=0, column=1, padx=5)
+        self.folder_entry.grid(row=0, column=1, padx=5, sticky=tk.EW)
 
         folder_btn = self.create_btn("폴더 찾기", self.select_folder, master=select_frame)
         folder_btn.grid(row=0, column=2, padx=5)
@@ -61,14 +57,42 @@ class BackupFolderApp:
         self.calendar = DateEntry(select_frame, font=self.app_font, foreground='white', borderwidth=2, date_pattern='yyyy-mm-dd')
         self.calendar.grid(row=1, column=1, sticky=tk.EW, padx=5, pady=5)
         
+        # 생성할 폴더 영역
+        target_folder_label = tk.Label(select_frame, text="생성할 폴더 : ", font=self.app_font)
+        target_folder_label.grid(row=2, column=0, sticky=tk.NE, padx=5, pady=5)
+        
+        target_folder_frame = tk.Frame(select_frame)
+        target_folder_frame.grid(row=2, column=1, sticky=tk.EW, padx=5, pady=5)
+        target_folder_frame.rowconfigure(0, weight=1)
+        target_folder_frame.columnconfigure(0, weight=1)
+        
+        self.scrollbar = tk.Scrollbar(target_folder_frame, width=20)
+        
+        self.listbox = tk.Listbox(target_folder_frame, yscrollcommand=self.scrollbar.set, font=self.app_font, height=6)
+        self.listbox.grid(row=0, column=0, sticky=tk.EW)
+        
+        self.scrollbar.grid(row=0, column=1, sticky="ns")
+        self.scrollbar.config(command=self.listbox.yview)
+        
+        listbox_btn_frame = tk.Frame(target_folder_frame)
+        listbox_btn_frame.grid(row=1, column=0, sticky=tk.EW)
+        listbox_btn_frame.columnconfigure(0, weight=1)
+        listbox_btn_frame.columnconfigure(1, weight=1)
+        
+        add_btn = self.create_btn("추가", self.add_to_listbox, master=listbox_btn_frame)
+        add_btn.grid(row=0, column=0, sticky=tk.EW)
+        
+        remove_btn = self.create_btn("삭제", lambda: print("remove"), master=listbox_btn_frame)
+        remove_btn.grid(row=0, column=1, sticky=tk.EW)
+        
         # 확인, 닫기 버튼
-        button_frame = tk.Frame(self.root)
-        button_frame.pack(pady=10)
+        btn_frame = tk.Frame(self.root)
+        btn_frame.pack(pady=(30, 0))
 
-        confirm_btn = self.create_btn("확인", lambda: print("confirm"), master=button_frame)
+        confirm_btn = self.create_btn("확인", lambda: print("confirm"), master=btn_frame)
         confirm_btn.pack(side=tk.LEFT, padx=20)
 
-        close_btn = self.create_btn("닫기", self.root.quit, master=button_frame)
+        close_btn = self.create_btn("닫기", self.root.quit, master=btn_frame)
         close_btn.pack(side=tk.LEFT, padx=20)
     
     # ───────────────────── 버튼 생성 ─────────────────────
@@ -93,6 +117,12 @@ class BackupFolderApp:
             print("선택한 폴더 경로:", folder_path)
             self.folder_entry.delete(0, tk.END)
             self.folder_entry.insert(0, folder_path)
+            
+    # ───────────────────── 리스트박스에 항목 추가 ─────────────────────
+    def add_to_listbox(self):
+        value = tk.simpledialog.askstring("추가", "추가할 폴더 이름을 입력하세요.")
+        if value:
+            self.listbox.insert(tk.END, value)
 
 
 # 앱 실행
